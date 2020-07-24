c176f6f7ab2c8667b722ab793cf3ae9afc334d8b
dihim <dihim@google.com>
2020-07-24 21:08:15 +0000

    Implement edit comment class

--- src/main/java/com/google/sps/data/EditComment.java
+++ src/main/java/com/google/sps/data/EditComment.java
@@ -15,7 +15,49 @@
 package com.google.sps.data;
 import java.util.Date;
 
-/** Edit Comment Class containing Wikipedia comment information. */
+/** Class containing server statistics. */
 public final class EditComment {
- 
+  
+  private final String userName;
+  private final String comment;
+  private final String date;
+  private final String parentArticle;
+  private final String status;
+  private final Integer revisionId;
+  private final String toxicityObject;
+
+  public EditComment(Integer revisionId, String userName, String comment, String toxicityObject, String date, String parentArticle, String status) {
+    this.revisionId = revisionId;
+    this.userName = userName;
+    this.comment = comment;
+    this.date = date;
+    this.parentArticle = parentArticle;
+    this.status = status;
+    this.toxicityObject = toxicityObject;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public String getParentArticle() {
+    return parentArticle;
+  }
+
+  public String getToxicityObject() {
+    return toxicityObject;
+  }
+
 }
