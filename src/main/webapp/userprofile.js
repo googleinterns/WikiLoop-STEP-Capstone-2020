786d1fe98cc89241c0179870a94a9099b2f51c6a
dihim <dihim@google.com>
2020-07-24 18:28:49 +0000

    Added removed files

---
+++ src/main/webapp/userprofile.js
@@ -0,0 +1,28 @@
+// initializes the table
+$(document).ready( function () {
+    $('#my-table').DataTable();
+} );
+
+/* TO DO (DEUS):   Use real data once David pushes the code for the EditComment class*/
+function getUser() {
+  fetch('/user').then(response => response.json()).then((user) => {
+    const userNameSection = document.getElementById('user-name');
+    userNameSection.innerText = user.userName;
+    const userPersonalInformationSection = document.getElementById('personal-information-box');
+    userPersonalInformationSection.innerText = "Personal Information (Just an ID for now): "+ user.id;
+    const avgToxicityScore = document.getElementById('toxicity-score-box');
+    avgToxicityScore.innerText= "Average Toxicity Score: \t" + user.avgToxicityScore;
+
+    // Build the list of edits
+    user.list_edit_comments.forEach((edit) => {
+      createEditElement(edit);
+    });
+  });
+}
+
+// Creates a row containing an edit in the table.
+function createEditElement(edit) {
+  var table = $('#my-table').DataTable();
+ 
+  table.row.add( ["50%", "Tom","www.fakeaticle.com",edit,"July 23, 2020"]).draw();
+}
