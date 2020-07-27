cf7a303b2dca23902de32e385f4e828470b1eee1
baelul <baelulhaile@google.com>
2020-07-27 18:16:11 +0000

    Add mock data and change frontend to match Discover page

--- src/main/java/com/google/sps/servlets/RetrieveEditServlet.java
+++ src/main/java/com/google/sps/servlets/RetrieveEditServlet.java
@@ -10,7 +10,9 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 
 //classes
+import com.google.sps.tests.MockData;
 import com.google.sps.data.EditComment;
+import com.google.sps.data.MockComment;
 
 //servlet
 import javax.servlet.annotation.WebServlet;
@@ -19,7 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//get comment id based on what is clicked in discover page
+//get comment based on what is clicked in discover page
 @WebServlet("/retrieve")
 public class RetrieveEditServlet extends HttpServlet {
 
@@ -27,23 +29,7 @@ public class RetrieveEditServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      //should give the comment with the same ID/Key as the request
-      /*
-      Query query = new Query("EditComment").setFilter(FilterPredicate(Entity.KEY_RESERVED_PROPERTY, 
-      FilterOperator.EQUAL, id));
-      PreparedQuery pq = datastore.prepare(query);
-      Entity results = pq.asSingleEntity();
-
-      String editComment = request.getParameter('editCommentText');
-
-      Gson gson = new Gson();
-      String json = gson.toJson(edit);
-      response.setContentType("application/json");
-      response.getWriter().println(gson.toJson(json));
-      */
-
-      //key from datastore 
-    //  Entity editComment = datastore.get(key);
+      
       
     }        
 }
