bb3f54d07d659b87d9eadbcf8502f0e15154d672
baelul <baelulhaile@google.com>
2020-08-10 12:06:07 +0000

    New Branch

--- src/main/webapp/edit-comment-script.js
+++ src/main/webapp/edit-comment-script.js
@@ -4,25 +4,37 @@ function getDetailedEdit() {
         const username = document.getElementById('username');
         const edit = document.getElementById('edit-comment');
         const date = document.getElementById('date');
-        const score = document.getElementById('toxic-score');
+        const score = document.getElementById('score');
+        const scoreLabel = document.getElementById('label');
+        const scoreReason = document.getElementById('reason');
+        const scoreIsExperimental = document.getElementById('experimental');
 
         article.setAttribute("href", detailedEdit.parentArticle);
         article.innerText = "Article Link";
         username.innerText = detailedEdit.userName;
         edit.innerText = detailedEdit.comment;
         date.innerText = detailedEdit.date;
-        score.innerText = detailedEdit.toxicityObject;
+        scoreLabel.innerText = detailedEdit.toxicityObject.label;
+        score.innerText = detailedEdit.toxicityObject.score;
+        scoreReason.innerText = detailedEdit.toxicityObject.reason;
+        scoreIsExperimental.innerText = experimentalMessage(detailedEdit.toxicityObject.scoreIsExperimental);
     });
 }
 
-/* psuedo code
-function changeStatus() {
-    if remove is pressed {
-        status = removed;
-    } else if suggest ban is pressed {
-        status = ban;
-    } else if warn is pressed {
-        status = warn;
+function experimentalMessage(boolAns) {
+    if (boolAns == 'true') {
+        return "This is an experimental score from the Perspective API."
+    } else {
+        return "This is not an experimental score."
     }
 }
-*/
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
