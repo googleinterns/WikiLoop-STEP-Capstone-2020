1de2907ec2b86d1173b57abba5340b05ce49673b
baelul <baelulhaile@google.com>
2020-08-25 16:26:11 +0000

    FIx incorrect fetch servlet (/actions)

--- src/main/webapp/status.js
+++ src/main/webapp/status.js
@@ -1,13 +1,13 @@
 function getActions() {
   fetch('/actions') .then(response => response.json()).then((actions) => { 
     const actionList = document.getElementById('container');
-    actions.forEach((status) => {
-        actionList.appendChild(createActionListElem(status));
+    actions.forEach((a) => {
+        actionList.appendChild(createActionListElem(a));
     })
   });
 }
 
-function createActionListElem(status) {
+function createActionListElem(a) {
     const liElem = document.createElement('li');
     liElem.classname = 'w3-bar';
 
@@ -15,19 +15,19 @@ function createActionListElem(status) {
 
     const user = document.createElement('a');
     user.setAttribute('id', 'user');
-    user.innerText = status.user;
+    user.innerText = a.user;
 
     const edit = document.createElement('a');
     edit.setAttribute('id', 'edit');
-    edit.innerText = status.revisionId;
+    edit.innerText = a.revisionId;
 
     const btn = document.createElement('a');
     btn.setAttribute('id', 'action');
-    action.innerText = status.action;
+    action.innerText = a.action;
 
     const time = document.createElement('a');
     time.setAttribute('id', 'time');
-    const timeDif = System.currentTimeMillis() - status.time;
+    const timeDif = System.currentTimeMillis() - a.time;
     time.innerText = timeDif;
 
     const statement = `${user}reviewed${edit}and says${action}about${time}ago.`;
